57217e33b3d900078f4829e47f6471b58d0db731
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-04-06 11:32:10 -0300

    class.

--- Criando-Objetos-e-Classes-em-JS/aula.js
+++ Criando-Objetos-e-Classes-em-JS/aula.js
@@ -1,13 +1,20 @@
 
-const person = {
-    name: 'Jaélson Freitas',
-    age: 33,
+class person {
+    name;
+    age;
 
-    toDescribe : function () {
-        console.log(`My name is ${this.name} and my age is ${this.age} years.`)
+    toDescribe(){
+        console.log(`My name is ${this.name} and my age is ${this.age} years.`);
     }
-};
+}
 
-const atribute = 'name'
 
-console.log(person[atribute]);
+const jaelson = new person();
+jaelson.name = 'Jaelson';
+jaelson.age = 33;
+
+const catarina = new person();
+catarina.name = 'Catarina';
+catarina.age = 52;
+
+console.log(catarina); 
