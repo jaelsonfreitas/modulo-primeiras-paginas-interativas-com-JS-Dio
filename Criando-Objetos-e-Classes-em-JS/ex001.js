0a5301e3ec37db50f551124fe26791e48a1889d8
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-04-06 12:21:25 -0300

    creating file and class.

---
+++ Criando-Objetos-e-Classes-em-JS/ex001.js
@@ -0,0 +1,11 @@
+/* 
+Crie uma classe para representar carros.
+Os carros possuem uma marca, uma cor , e um gasto médio de combustivel por KM rodado.
+Crie um método que dado a quantidade de km e gasto de combustivel nos forneça o valor gasto para percorrer esse trajeto.*/
+
+class cars {
+    brand;
+    color;
+    averageCostPerKilometer;
+}
+
