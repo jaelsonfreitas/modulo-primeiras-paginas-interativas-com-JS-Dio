a13f4395d3d84cbded82971a594038a8c2f55383
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-04-06 12:07:48 -0300

    exercise.

--- Criando-Objetos-e-Classes-em-JS/aula.js
+++ Criando-Objetos-e-Classes-em-JS/aula.js
@@ -15,8 +15,17 @@ class person {
     }
 }
 
+function comparePerson(p1,p2) {
+    if (p1.age > p2.age) {
+        console.log(`${p1.name} is older than ${p2.name}`);
+    } else if (p1.age < p2.age) {
+        console.log(`${p2.name} is older than ${p1.name}`);   
+     } else {
+        console.log(`${p2.name} is the same age as ${p1.name}`);  
+     }
+}
 
-const jaelson = new person('Jaélson', 33);
-const catarina = new person('Catarina', 52);
+const jaelson = new person('Jaelson', 30);
+const thainah = new person ('Thainah', 33);
 
-console.log(jaelson);
+comparePerson(thainah, jaelson)
