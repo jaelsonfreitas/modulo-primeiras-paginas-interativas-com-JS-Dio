ed627b2802225eaff7860120a876d8d1484e5b4c
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-04-07 22:09:42 -0300

    complete exercise

--- Criando-Objetos-e-Classes-em-JS/ex002.js
+++ Criando-Objetos-e-Classes-em-JS/ex002.js
@@ -19,8 +19,22 @@ class persons {
      calculateIbm(){
        return this.weight/(this.age * this.age);
      }
+     classifyBmi(){
+        const imc = this.calculateIbm();
+        if (imc < 18.5) {
+            return ('Abaixo do peso');
+        } else if ( imc >= 18.5 && imc < 25) {
+            return ('Peso normal');
+        } else if (imc >= 25 && imc < 30) {
+            return ('Acima do peso');
+        }  else if (imc >= 30 && imc < 40) {
+            return ('Obeso');
+        } else {
+            return ('Obesidade grave');
+        }
+     }
 }
 const jose = new persons('José', 70, 1.75);
 const thainah = new persons('Thainah', 82, 1.69)
-console.log(jose.calculateIbm());
-console.log(thainah.calculateIbm()); 
+console.log(jose.classifyBmi());
+console.log(thainah.classifyBmi()); 
