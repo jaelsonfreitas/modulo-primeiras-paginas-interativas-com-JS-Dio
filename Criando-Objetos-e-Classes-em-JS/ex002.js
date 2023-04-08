47ac923509182cc1d7167312d574f3f810f76d02
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-04-07 21:51:05 -0300

    calculate IBM..

---
+++ Criando-Objetos-e-Classes-em-JS/ex002.js
@@ -0,0 +1,22 @@
+/*
+2 - Crie uma classe para representar pessoas.
+    Para cada pessoa teremos os atributos : nome, peso, e altura.
+    As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC = peso/(altura*altura));
+    Instancie uma pessoa chamada Jośe que tenha 70 kg de peso e 1,75 de altura e peça para José dizer o valor do seu IMC.
+*/
+
+class persons {
+    name;
+    weight;
+    age;
+
+    constructor(name,weight,age) {
+        this.name = name
+        this.weight = weight
+        this.age = age
+    }
+
+     calculateIbm(){
+       return this.weight/(this.age * this.age);
+     }
+}
