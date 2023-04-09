47ab67df59e556fa2362d547a0677f19afd33311
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-04-08 22:48:59 -0300

    I understand the basics

---
+++ Arrays-e-Estruturas-de-Repeticao-em-JS/for.js
@@ -0,0 +1,18 @@
+
+
+
+for (let index = 0; index < 11; index++) {
+    console.log(index);
+    
+}
+
+
+/*
+    O for de dividido em tres partes:
+    let index = 0;
+    A primeira parte é a definição do nosso Index da nossa variável de controle.
+    index < array.length;
+    A segunda parte é a estrutura condicional, vai dizer se entra ou não dentro do for.
+    index ++
+    A terceira parte nos diz o que vai acontecer depois que terminar de executar o nosso for.
+*/
