a0c31405ec0a68218454159cf545cc48bcb909dd
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-04-08 22:22:15 -0300

    . push

---
+++ Arrays-e-Estruturas-de-Repeticao-em-JS/aula1.js
@@ -0,0 +1,9 @@
+
+
+
+const students = ['João', 'Maria', 'Vitor']
+/* Para adicionar itens na lista utilizamos o .push. */
+students.push('Renan')
+/* Também posso adicionar diretamente a posição como no exemplo abaixo.*/
+students[4] = 'Bryan'
+console.log(students);
