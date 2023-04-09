846e02da294b5bf9f46ef3959889423a991fef70
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-04-08 22:35:59 -0300

    .shift .pop .lenght

--- Arrays-e-Estruturas-de-Repeticao-em-JS/aula1.js
+++ Arrays-e-Estruturas-de-Repeticao-em-JS/aula1.js
@@ -7,3 +7,8 @@ students.push('Renan')
 /* Também posso adicionar diretamente a posição como no exemplo abaixo.*/
 students[4] = 'Bryan'
 console.log(students);
+
+/* Também temo os .pop que retira a ultima posiçao mais proxima.
+    Temos o .shift que retira a primeira posição. 
+    .lenght devolve o tamanho da lista.
+    */
