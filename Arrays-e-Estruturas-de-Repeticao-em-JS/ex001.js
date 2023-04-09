4a4b898cfa7e02366d1fc65f68d5f6b8689fc7d4
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-04-08 23:51:09 -0300

    multiplication table

---
+++ Arrays-e-Estruturas-de-Repeticao-em-JS/ex001.js
@@ -0,0 +1,9 @@
+/*
+1 - Crie um programa que dado um  numero imprima a sua tabuada.
+*/
+
+const multiplicationTable = 7
+ 
+for (let index = 1; index <= 10 ; index++) {
+   console.log(index * 7);
+}
