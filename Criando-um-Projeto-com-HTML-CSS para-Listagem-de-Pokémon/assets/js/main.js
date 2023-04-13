dca3143466101e3dd99a84218239dd32ccd9c4b9
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-04-13 12:13:02 -0300

    Manipulando resultado da requisição através de uma Promise.

--- Criando-um-Projeto-com-HTML-CSS para-Listagem-de-Pokémon/assets/js/main.js
+++ Criando-um-Projeto-com-HTML-CSS para-Listagem-de-Pokémon/assets/js/main.js
@@ -4,6 +4,7 @@ const offset = 0
 const limit = 0
 const url = `https://pokeapi.com/api/v2/pokemon?offset=${offset}&${limit}`
 
-fetch(url).then(function(response){
-    console.log(response);
-})
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.error(error))
